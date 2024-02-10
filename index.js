1ab1765c26e8c2154dc4adc47267835f5fe205ec
Hana Spoustová <bernreih@gmail.com>
2024-02-10 23:58:19 +0100

    reseni

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+const changeSrc = () => {
+  side += 1;
+  if (side == 7) {
+    side = 1;
+  }
 
+  picture.src = 'img/side' + side + '.svg';
+};
+
+let side = 1;
+
+const picture = document.querySelector('.dice');
+picture.addEventListener('click', changeSrc);
